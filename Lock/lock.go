82cb48e167f65f3b355569d81e40c5a87ef7c6f6
smx_Moragn <1048492656@qq.com>
2023-03-31 20:48:13 +0800

    redis自动续约问题

--- Lock/lock.go
+++ Lock/lock.go
@@ -32,26 +32,83 @@ func (c *Client) TryLock(ctx context.Context, key string, expiration time.Durati
 	if !res {
 		return nil, ErrFailedTOPreemptLock
 	}
-	return newLock(c.client, key, value), nil
+	return newLock(c.client, key, value, expiration), nil
 }
 
 type Lock struct {
-	client redis.Cmdable
-	Key    string
-	Value  string
+	client     redis.Cmdable
+	Key        string
+	Value      string
+	expiration time.Duration
+	unlock     chan struct{}
 }
 
-func newLock(client redis.Cmdable, key string, value string) *Lock {
+func newLock(client redis.Cmdable, key string, value string, expiration time.Duration) *Lock {
 	return &Lock{
-		client: client,
-		Key:    key,
-		Value:  value,
+		client:     client,
+		Key:        key,
+		Value:      value,
+		expiration: expiration,
+		unlock:     make(chan struct{}, 1),
 	}
+}
+func (l *Lock) AutoRefresh(interval time.Duration, timeout time.Duration) error {
+	ch := make(chan struct{}, 1)
+	defer close(ch)
+	ticker := time.NewTicker(interval)
+	for {
+		select {
+		case <-ch:
+			ctx, cancel := context.WithTimeout(context.Background(), timeout)
+			err := l.Refresh(ctx)
+			cancel()
+			if err == context.DeadlineExceeded {
+				//立即重试
+				ch <- struct{}{}
+				continue
+			}
+			if err != nil { //始终需要处理
+				return err
+			}
+		case <-ticker.C:
+			ctx, cancel := context.WithTimeout(context.Background(), timeout)
+			err := l.Refresh(ctx)
+			cancel()
+			if err == context.DeadlineExceeded {
+				//立即重试
+				ch <- struct{}{}
+				continue
+			}
+			if err != nil { //始终需要处理
+				return err
+			}
+		case <-l.unlock:
+			return nil
+
+		}
+	}
+}
+func (l *Lock) Refresh(ctx context.Context) error {
+	res, err := l.client.Eval(ctx, luaRefresh, []string{l.Key}, l.Value, l.expiration.Milliseconds()).Int64()
+	if redis.Nil == err {
+		return ErrLockNotHold
+	}
+	if err != nil {
+		return err
+	}
+	//判断res是否是1
+	if res == 1 {
+		//这个锁不存在或者不是你的
+		return ErrLockNotHold
+	}
+	return nil
 }
 
 var (
 	//go:embed unlock.lua
 	luaUnlock string
+	//go:embed refresh.lua
+	luaRefresh string
 )
 
 func (l *Lock) UnLock(ctx context.Context) error {
@@ -80,6 +137,10 @@ func (l *Lock) UnLock(ctx context.Context) error {
 	*/
 
 	res, err := l.client.Eval(ctx, luaUnlock, []string{l.Key}, l.Value).Int64()
+	defer func() {
+		l.unlock <- struct{}{}
+		close(l.unlock)
+	}()
 	if redis.Nil == err {
 		return ErrLockNotHold
 	}
